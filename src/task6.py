88c1ac8cda3c49bd8d5fe852f5c2a7696074e8cd
piology <pius.grossmann@web.de>
2026-06-10 15:48:43 +0200

    Implement naive JFA using the square euclidean distance

--- src/task6.py
+++ src/task6.py
@@ -1,13 +1,125 @@
+import os
 import numpy as np
 from numba import cuda
+import imageio.v3 as imageio
 from matplotlib import pyplot as plt
 
 
-@cuda.jit
-def _jfa_pass_kernel(grid_in, grid_out, step_size, size) -> None:
+from constants import DATA_FOLDER
+from utils import (
+    make_grid_configuration,
+    calculate_square_euclidean_distance,
+)
+
+# Resolution of the image containing the voronoi diagram
+RESOLUTION: int = 512  # 1024
+
+# The number of seeds (points) in the diagram
+SEED_COUNT: int = 200  # 2000
+
+
+def main() -> None:
+    """
+    Main entry point.
+    Test the implementations and generate example diagrams for the Jump Flooding Algorithm (JFA).
+    """
+
+    # Naive Euclidean JFA
+    image = jfa_naive_euclidean(
+        seeds=generate_random_seeds_jfa(seed_count=SEED_COUNT, resolution=RESOLUTION),
+        resolution=RESOLUTION,
+    )
+    plt.imshow(image)
+    plt.show()
+
+
+def generate_random_seeds_jfa(seed_count: int, resolution: int) -> np.ndarray:
+    """
+    Generate a random array of seed positions within the resolution range.
+    The array contains seed_count entries, with each entry representing the x and y position of a seed within the grid (integer).
+    """
+
+    return np.random.randint(low=0, high=resolution, size=(seed_count, 2))
+
+
+def generate_grid_jfa(seeds: np.ndarray, resolution: int) -> np.ndarray:
+    """
+    Generate a grid data structure for the JFA.
+    Each pixel in the grid represents the x and y position of the nearest seed. During initialisation,
+    each pixel is given a default value of '-1', indicating that no seed position has been set.
+    At the start, each seed knows its position and is therefore set directly in the grid configuration.
+    """
+
+    # Construct and initialize a grid: Each pixel stores the position of the closest seed (seed_x, seed_y)
+    # Initialization of '-1' means: "No seed known yet"
+    grid: np.ndarray = np.full(
+        shape=(resolution, resolution, 2), fill_value=-1, dtype=np.int32
+    )
+
+    # Set the positions of the seeds in the grid
+    # Each seed knows its own position at the start
+    for seed_x, seed_y in seeds:
+        grid[seed_y, seed_x, 0] = seed_x
+        grid[seed_y, seed_x, 1] = seed_y
+
+    return grid
+
+
+def jfa_naive_euclidean(
+    seeds: cuda.devicearray.DeviceNDArray, resolution: int
+) -> np.ndarray:
+    """
+    Host function that sets everything up for the naive JFA using the Euclidean distance calculation.
+    """
+
+    # GPU grid allocation for the ping pong
+    grid_in = cuda.to_device(generate_grid_jfa(seeds=seeds, resolution=resolution))
+    grid_out = cuda.device_array_like(grid_in)
+
+    # Grid configuration
+    blocks_per_grid, threads_per_block = make_grid_configuration(
+        resolution=resolution, threads_per_dimension=16
+    )
+
+    # Jump Flooding Loop (Ping Pong)
+    # The step size starts at N/2 and is halved with each step until it reaches 1
+    k: int = resolution // 2
+    while k >= 1:
+        # Kernel launch
+        _jfa_pass_naive_kernel[blocks_per_grid, threads_per_block](
+            grid_in, grid_out, k, resolution
+        )
+
+        # Ping Pong: Swap the grids so that the previous result becomes the new input
+        grid_in, grid_out = grid_out, grid_in
+
+        # Divide further
+        k //= 2
+
+    # Retrieve the final result
+    # Since the values are swapped at the end of the loop, grid_in contains the data from the last iteration
+    out_image = grid_in.copy_to_host()
+
+    # Each seed is assigned a unique ID
+    return out_image[:, :, 0] + out_image[:, :, 1] * resolution
+
+
+@cuda.jit("void(int32[:,:,:], int32[:,:,:], int32, int32)")
+def _jfa_pass_naive_kernel(grid_in, grid_out, step_size, size) -> None:
+    """
+    Executes a single pass of the Jump Flooding Algorithm (JFA) on the GPU.
+
+    This naive kernel evaluates the current pixel's seed and checks its 8 grid
+    neighbors at the specified step size distance. If a neighbor point references
+    a closer seed (using squared Euclidean distance), the pixel updates its
+    tracked seed coordinates.
+    """
+
+    # Determine the thread's position within the 2D grid
     pixel_x, pixel_y = cuda.grid(2)
 
-    # Out of bounds
+    # Out of bounds check: Terminate threads outside the valid image boundaries
+    # if is_outside_image(pixel_x, pixel_y, grid_in):
     if pixel_x >= size or pixel_y >= size:
         return
 
@@ -16,10 +128,14 @@ def _jfa_pass_kernel(grid_in, grid_out, step_size, size) -> None:
     best_seed_y = grid_in[pixel_y, pixel_x, 1]
 
     # Calculate initial distance
-    best_dist = float("inf")
-    # NOTE: Only update if the default start value is not -1
+    best_dist: float = float("inf")
+
+    # Only update distance if the current pixel already knows a valid seed
     if best_seed_x != -1 and best_seed_y != -1:
-        best_dist = (pixel_x - best_seed_x) ** 2 + (pixel_y - best_seed_y) ** 2
+        # best_dist = (pixel_x - best_seed_x) ** 2 + (pixel_y - best_seed_y) ** 2
+        best_dist = calculate_square_euclidean_distance(
+            pixel_x, pixel_y, best_seed_x, best_seed_y
+        )
 
     # Look for all eight neighbours with the current step size k (north, west, east, south, and the four diagonals)
     for dy in (-1, 0, 1):
@@ -34,10 +150,13 @@ def _jfa_pass_kernel(grid_in, grid_out, step_size, size) -> None:
                 seed_x = grid_in[neighbour_y, neighbour_x, 0]
                 seed_y = grid_in[neighbour_y, neighbour_x, 1]
 
-                # Check if the neighbour already knows a seed (= not the initial default value of -1)
+                # Check if the neighbour already knows a seed (= does not have the initial default value of -1)
                 if seed_x != -1 and seed_y != -1:
                     # Calculate the distance from the current pixel to the seed that the neighbour knows
-                    dist = (pixel_x - seed_x) ** 2 + (pixel_y - seed_y) ** 2
+                    # dist = (pixel_x - seed_x) ** 2 + (pixel_y - seed_y) ** 2
+                    dist = calculate_square_euclidean_distance(
+                        pixel_x, pixel_y, seed_x, seed_y
+                    )
 
                     # Check whether the newly found seed is closer than the last one saved
                     if dist < best_dist:
@@ -46,59 +165,26 @@ def _jfa_pass_kernel(grid_in, grid_out, step_size, size) -> None:
                         best_seed_x = seed_x
                         best_seed_y = seed_y
 
-    # Write the result to the output grid (ping pong buffer)
+    # Write the closest seed coordinates to the output grid
     grid_out[pixel_y, pixel_x, 0] = best_seed_x
     grid_out[pixel_y, pixel_x, 1] = best_seed_y
 
 
-# Resolution
-N = 512
-
-# Construct and initialize a grid: Each pixel stores (seed_x, seed_y)
-# -1 means: "No seed known yet"
-grid = np.full(shape=(N, N, 2), fill_value=-1, dtype=np.int32)
-
-# Define an array of random seeds
-num_seeds = 100
-seeds = np.random.randint(low=0, high=N, size=(num_seeds, 2))
+def jfa_ping_pong_loop():
+    pass
 
 
-# Set the positions of the seeds in the grid
-# Each seed knows its own position at the start
-for seed_x, seed_y in seeds:
-    grid[seed_y, seed_x, 0] = seed_x
-    grid[seed_y, seed_x, 1] = seed_y
+def jfa_naive_manhattan():
+    pass
 
 
-# GPU allocation for the ping pong
-grid_in = cuda.to_device(grid)
-grid_out = cuda.device_array_like(grid_in)
-
-# Grid configuration
-threads_per_block = (16, 16)
-blocks_x = (N + threads_per_block[0] - 1) // threads_per_block[0]
-blocks_y = (N + threads_per_block[1] - 1) // threads_per_block[1]
-blocks = (blocks_x, blocks_y)
+def jfa_plus1_naive_euclidean():
+    pass
 
 
-# Jump Flooding Loop (Ping Pong)
-# The step size starts at N/2 and is halved with each step until it reaches 1
-k = N // 2
-while k >= 1:
-    # Kernel launch
-    _jfa_pass_kernel[blocks, threads_per_block](grid_in, grid_out, k, N)
-
-    # Ping Pong: Swap buffers so that the previous result becomes the new input
-    grid_in, grid_out = grid_out, grid_in
-
-    # Divide further
-    k //= 2
-
-# Retrieve the final result
-# Since the values are swapped at the end of the loop, grid_in contains the data from the last iteration
-result_grid = grid_in.copy_to_host()
+def jfa_plus2_naive_euclidean():
+    pass
 
 
-# Each seed is assigned a unique ID
-plt.imshow((result_grid[:, :, 0] + result_grid[:, :, 1] * N), cmap="prism")
-plt.show()
+if __name__ == "__main__":
+    main()
