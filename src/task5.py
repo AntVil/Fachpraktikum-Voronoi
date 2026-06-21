f76d82ff5427ba1e9f25d4a7253f50a79c10cc0b
AntVil <antboehler@gmail.com>
2026-06-21 12:58:21 +0200

    implement warp shfl variant

--- src/task5.py
+++ src/task5.py
@@ -18,6 +18,8 @@ GRID_STRIDE_SIZE = 128
 """
 Number of points which will be loaded inside grid-stride-loop (be aware that each point consists of 2 values)
 """
+WARP_SIZE = 32
+WARP_POINTS = WARP_SIZE // 2
 
 
 def main() -> None:
@@ -28,6 +30,13 @@ def main() -> None:
 
     if command == "euclidean-grid-stride":
         image = voronoi_euclidean_grid_stride(
+            points=generate_uniform_points(point_count=point_count),
+            resolution=resolution
+        )
+        plt.imshow(image)
+        plt.show()
+    elif command == "euclidean-warp-shfl":
+        image = voronoi_euclidean_warp_shfl(
             points=generate_uniform_points(point_count=point_count),
             resolution=resolution
         )
@@ -51,7 +60,7 @@ def voronoi_euclidean_grid_stride(
 
     assert np.prod(threads_per_block) > GRID_STRIDE_SIZE, f"threads_per_block={threads_per_block} and GRID_STRIDE_SIZE={GRID_STRIDE_SIZE} are incompatible, points would be missed, either increase threads_per_block or decrease GRID_STRIDE_SIZE."
 
-    _voroni_euclidean_grid_stride_kernel[blocks_per_grid, threads_per_block](
+    _voroni_euclidean_grid_stride_kernel[blocks_per_grid, threads_per_block]( # type: ignore
         points,
         out_image
     )
@@ -72,7 +81,7 @@ def _voroni_euclidean_grid_stride_kernel(
     (stride_offset_point, stride_offset_dimension) = get_thread_grid_stride_start()
 
     # NOTE: local buffer with same characteristics like `in_points`, just smaller
-    shared_buffer: cuda.devicearray.DeviceNDArray = cuda.shared.array(shape=(GRID_STRIDE_SIZE, 2), dtype=np.float64)
+    shared_buffer: cuda.devicearray.DeviceNDArray = cuda.shared.array(shape=(GRID_STRIDE_SIZE, 2), dtype=np.float64) # type: ignore
 
     for stride_index in range(0, in_points.shape[0], GRID_STRIDE_SIZE):
         # NOTE: check if thread is inside `shared_buffer`
@@ -105,6 +114,79 @@ def _voroni_euclidean_grid_stride_kernel(
                 closest_index = global_point_index
 
         cuda.syncthreads()
+
+    # NOTE: this cannot be done earlier as each thread now has more responsibilities
+    # (A thread might be outside the image, but still is required for loading data)
+    if is_outside_image(x_index=x_index, y_index=y_index, image=out_image):
+        return
+
+    out_image[x_index, y_index] = closest_index
+
+
+def voronoi_euclidean_warp_shfl(
+    points: cuda.devicearray.DeviceNDArray,
+    resolution: int
+) -> np.ndarray:
+    out_image = make_empty_voronoi_output(resolution=resolution)
+
+    blocks_per_grid, threads_per_block = make_grid_configuration(
+        resolution=resolution,
+        threads_per_dimension=16
+    )
+
+    assert np.prod(threads_per_block) % WARP_SIZE == 0, f"threads_per_block={threads_per_block} and WARP_SIZE={WARP_SIZE} are incompatible, points would be missed."
+
+    _voroni_euclidean_warp_shfl[blocks_per_grid, threads_per_block]( # type: ignore
+        points,
+        out_image
+    )
+
+    return out_image.copy_to_host()
+
+
+@cuda.jit("void(float64[:, :], int32[:, :])")
+def _voroni_euclidean_warp_shfl(
+    in_points: cuda.devicearray.DeviceNDArray,
+    out_image: cuda.devicearray.DeviceNDArray
+) -> None:
+    closest_index = 0
+    closest_distance = np.inf
+
+    (x_index, y_index, x_coordinate, y_coordinate) = get_thread_position(image=out_image)
+
+    (stride_offset_point, stride_offset_dimension) = get_thread_grid_stride_start()
+    stride_offset_point = stride_offset_point % WARP_POINTS
+
+    # NOTE: Container of `x` and `y` depending on `stride_offset_dimension`
+    point_component_warp_value = np.inf
+
+    for stride_index in range(0, in_points.shape[0], WARP_POINTS):
+        global_point_index = stride_index + stride_offset_point
+
+        # NOTE: check if point exists
+        if global_point_index < in_points.shape[0]:
+            point_component_warp_value = in_points[global_point_index, stride_offset_dimension]
+        else:
+            # NOTE: default value for missing points
+            point_component_warp_value = np.inf
+
+        # NOTE: Go through loaded data and do the actual algorithm
+        for index in range(0, WARP_SIZE, 2):
+            # NOTE: get `x` and `y` component from two consecutive threads of the warp
+            point_x_coordinate = cuda.shfl_sync(0xFFFFFFFF, point_component_warp_value, index)
+            point_y_coordinate = cuda.shfl_sync(0xFFFFFFFF, point_component_warp_value, index + 1)
+
+            distance = calculate_square_euclidean_distance(
+                x_coordinate = x_coordinate,
+                y_coordinate = y_coordinate,
+                point_x_coordinate = point_x_coordinate,
+                point_y_coordinate = point_y_coordinate
+            )
+
+            if distance < closest_distance:
+                closest_distance = distance
+                global_point_index = stride_index + (index // 2)
+                closest_index = global_point_index
 
     # NOTE: this cannot be done earlier as each thread now has more responsibilities
     # (A thread might be outside the image, but still is required for loading data)
